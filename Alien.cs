cf956d6b5cb614ab9b43e9b50f228962b762af08
Douglas Wachtel <douglas.wachtel@gmail.com>
2018-09-05 16:55:20 -0400

    Initial Upload - Models

---
+++ Alien.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardCreator
+{
+    public class Alien
+    {
+        public string Species { get; set; }
+        public int PointValue { get; set; }
+        public int Muscle { get; set; }
+        public int Skill { get; set; }
+        public int Brains { get; set; }
+        public int Charm { get; set; }
+        public bool isPassForHuman { get; set; }
+        public bool isAggressive { get; set; }
+        public bool isMultiLimbed { get; set; }
+        public bool isRavenous { get; set; }
+        public bool isGross { get; set; }
+        public string extraText { get; set; }
+
+    }
+}
---
+++ Boost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardCreator
+{
+    public class Boost
+    {
+        public string Name { get; set; }
+        public int MuscleAdj { get; set; }
+        public int SkillAdj { get; set; }
+        public int BrainsAdj { get; set; }
+        public int CharmAdj { get; set; }
+        public string SpecialText { get; set; }
+        public string FlavorText { get; set; }
+    }
+}
